7c8249a929db7f9fa5dc9485e1a43f9f96c9c597
wjx0820 <1989wjx@gmail.com>
2022-02-11 10:28:42 +0800

    set security headers via middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,9 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a
+// http.Handler instead of *http.ServeMux.
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -10,5 +12,9 @@ func (app *application) routes() *http.ServeMux {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
+
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return secureHeaders(mux)
 }
