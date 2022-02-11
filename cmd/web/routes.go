3149cc1449ee7fe46dffddd5f97aad6732efd05e
wjx0820 <1989wjx@gmail.com>
2022-02-11 10:58:04 +0800

    panic recovery via middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,6 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Wrap the existing chain with the logRequest middleware.
-	return app.logRequest(secureHeaders(mux))
+	// Wrap the existing chain with the recoverPanic middleware.
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
