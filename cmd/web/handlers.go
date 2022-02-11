44bb76bbfd12f59307848d8a992ff2ec46704290
wjx0820 <1989wjx@gmail.com>
2022-02-11 15:51:05 +0800

    extract form validation into form pkg

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strings"
-	"unicode/utf8"
 
 	"net/http"
 	"strconv"
 
+	"github.com/wjx0820/snippetbox/pkg/forms"
 	"github.com/wjx0820/snippetbox/pkg/models"
 )
 
@@ -51,7 +50,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // Add a new createSnippetForm handler, which for now returns a placeholder response.
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -61,53 +63,21 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	// Create a new forms.Form struct containing the POSTed data from the
+	// form, then use the validation methods to check the content.
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	// Initialize a map to hold any validation errors.
-	errors := make(map[string]string)
-
-	// Check that the title field is not blank and is not more than 100 characters
-	// long. If it fails either of those checks, add a message to the errors
-	// map using the field name as the key.
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field is too long (maximum is 100 characters)"
-	}
-
-	// Check that the Content field isn't blank.
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field cannot be blank"
-	}
-
-	// Check the expires field isn't blank and matches one of the permitted
-	// values ("1", "7" or "365").
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
-
-	// If there are any validation errors, re-display the create.page.tmpl
-	// template passing in the validation errors and previously submitted
-	// r.PostForm data.
-	if len(errors) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData{FormErrors: errors,
-			FormData: r.PostForm,
-		})
+	// If the form isn't valid, redisplay the template passing in the
+	// form.Form object as the data.
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	// If there are any errors, dump them in a plain text HTTP response and return
-	// from the handler.
-	if len(errors) > 0 {
-		fmt.Fprint(w, errors)
-		return
-	}
-
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"github.com/wjx0820/snippetbox/pkg/forms"
 	"github.com/wjx0820/snippetbox/pkg/models"
 )
 
-// Add FormData and FormErrors fields to the templateData struct.
+// Update the templateData fields, removing the individual FormData and
+// FormErrors fields and replacing them with a single Form field.
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
